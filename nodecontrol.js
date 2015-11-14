1f67a34e3a063ceccaf150e348dd96b9ef3d2f9a
philzook58 <philip@FartMachine7.local>
2015-11-14 17:42:45 -0500

    some bull

--- nodecontrol.js
+++ nodecontrol.js
@@ -26,8 +26,8 @@ http.get({
            // Data reception is done, do whatever with it!
            var parsed = JSON.parse(body);
            console.log(parsed);
-           var x = floor((parsed.x - 0.5) * 2 * multiplier);
-           var y = floor((parsed.y - 0.5) * 2 * multiplier);
+           var x = Math.floor((parsed.x - 0.5) * 2 * multiplier);
+           var y = Math.floor((parsed.y - 0.5) * 2 * multiplier);
            console.log(x);
            serialPort.write(x + "x\r\n" + y + "y\r\n", function(err, results) {
 
