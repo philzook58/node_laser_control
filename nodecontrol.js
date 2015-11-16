453effe5c5432f9a276ac87e2ebaa943f24a5e67
Ben <ishmandoo@gmail.com>
2015-11-15 23:54:22 -0500

    fixed nodecontrol, added socket control

--- nodecontrol.js
+++ nodecontrol.js
@@ -2,11 +2,14 @@ var http = require('http');
 
 var requestTime = 50;
 var multiplier = 100;
-var port = "/dev/ttyACM0"
+var port = "/dev/ttyUSB0"
+
+var x = 0;
+var y = 0;
 
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort(port, {
-  baudrate: 19200
+  baudrate: 115200
 });
 
 
@@ -26,9 +29,19 @@ http.get({
            // Data reception is done, do whatever with it!
            var parsed = JSON.parse(body);
            console.log(parsed);
-           var x = (parsed.x - 0.5) *2 * multiplier;
-           var y = (parsed.y - 0.5) *2 * multiplier;
-           console.log(x);
+           if (parsed.users > 0) {
+
+             x = (parsed.x - 0.5) *2 * multiplier;
+             y = (parsed.y - 0.5) *2 * multiplier;
+             console.log(x);
+           } else {
+             x = x + 10*(Math.random() - 0.5);
+             y = y + 10*(Math.random() - 0.5);
+             if ((x > multiplier) || (x < -multiplier) || (y > multiplier) || (y < -multiplier)) {
+               x = 0
+               y = 0;
+             }
+           }
            serialPort.write(x + "," + y + ";", function(err, results) {
 
 
--- socketcontrol.js
+++ socketcontrol.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var socket = require('socket.io-client')('http://45.55.243.197');
+var socket = require('socket.io-client')('http://45.55.243.297');
 var multiplier = 100;
 var fs = require('fs');
 var port = "/dev/ttyACM0"
@@ -8,7 +8,21 @@ var mystream = fs.createWriteStream(port,{flags:'w'});
 socket.on('connect', function(){});
 
 socket.on('broadcast pos', function(data){
-  var x = (parsed.x - 0.5) * 2 * multiplier;
-  var y = (parsed.y - 0.5) * 2 * multiplier;
+  console.log(data)
+  var x = (data.x - 0.5) * 2 * multiplier;
+  var y = (data.y - 0.5) * 2 * multiplier;
   mystream.write(x+','+y+";");
 });
+
+/*
+writePos1();
+function writePos1() {
+  mystream.write(3+','+3+";");
+  setInterval(writePos2, 100);
+}
+
+function writePos2() {
+  mystream.write(0+','+0+";");
+  setInterval(writePos1, 100);
+}
+*/
